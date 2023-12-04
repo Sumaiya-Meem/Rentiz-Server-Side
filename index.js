62460282df4fce3d4f198f610ccea474b0aa10fb
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2023-12-04 10:03:51 +0600

    connect localhost

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+// require('dotenv').config()
+const port = process.env.PORT || 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Rentiz Website is running .....')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Rentiz Website  is running on port ${port}`)
+  })
